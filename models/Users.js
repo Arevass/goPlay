b21952842b1a3b19448ce07b112c08d1d041c425
Arevass <glennderwin@gmail.com>
2017-03-23 02:50:36 +0000

    Alt auth branch

--- models/Users.js
+++ models/Users.js
@@ -6,6 +6,9 @@ var uniqueValidator = require('mongoose-unique-validator');
 var UserSchema = new mongoose.Schema({
 
     username: {type: String, lowercase: true, required: [true, "can't be blank"], index: true, match: [/^[a-zA-Z0-9]+$/, 'is invalid']},
+    email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    bio: String,
+    image: String,
     logins: {type: Number, default: 0},
     hash: String,
     salt: String,
@@ -18,7 +21,7 @@ UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 UserSchema.methods.setPassword = function (password) {
 
     this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('hex');
 
 };
 
